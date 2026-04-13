7ce105e1102522afde9a86e9771f5c2fa1ed24d5
Student <ST10518031@rcconnect.edu.za>
2026-04-13 11:37:32 +0200

    3rd commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hey");
+
         Registration reg = new Registration();
         String regMessage = reg.registerUser();
         System.out.println(regMessage);
