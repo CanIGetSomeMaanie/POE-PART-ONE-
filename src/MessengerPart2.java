3119028927b804ecf6b101df55034df6d187865a
CanIGetSomeMaanie <ST10518031@rcconnect.edu.za>
2026-06-13 11:50:50 +0200

    Update MessengerPart2.java

--- src/MessengerPart2.java
+++ src/MessengerPart2.java
@@ -52,4 +52,93 @@ public class MessengerPart2 {
         assertEquals(
                 "Cell phone number is incorrectly formatted or does not contain an international code. Please correct the number and try again.",
                 MainPart2.checkRecipientCell(recipient));
+    }
+    @Test
+    public void testLongestMessage() {
+
+        MainPart2.storedMessages.clear();
+
+        MainPart2.storedMessages.add(
+                "Did you get the cake?");
+
+        MainPart2.storedMessages.add(
+                "Where are you? You are late! I have asked you to be on time.");
+
+        assertEquals(
+                "Where are you? You are late! I have asked you to be on time.",
+                MainPart2.getLongestMessage());
+    }
+
+    @Test
+    public void testSearchRecipient() {
+
+        MainPart2.recipients.clear();
+        MainPart2.storedMessages.clear();
+
+        MainPart2.recipients.add(
+                "+27838884567");
+
+        MainPart2.storedMessages.add(
+                "Did you get the cake?");
+
+        assertEquals(
+                "+27838884567",
+                MainPart2.recipients.get(0));
+    }
+
+    @Test
+    public void testMessageArrayPopulated() {
+
+        MainPart2.sentMessages.clear();
+
+        MainPart2.sentMessages.add(
+                "Did you get the cake?");
+
+        MainPart2.sentMessages.add(
+                "It is dinner time!");
+
+        assertEquals(
+                2,
+                MainPart2.sentMessages.size());
+    }
+
+    @Test
+    public void testDeleteMessageHash() {
+
+        MainPart2.messageHashes.clear();
+        MainPart2.storedMessages.clear();
+
+        MainPart2.messageHashes.add(
+                "Test Message 2");
+
+        MainPart2.storedMessages.add(
+                "Where are you? You are late! I have asked you to be on time.");
+
+        MainPart2.deleteMessageHash(
+                "Test Message 2");
+
+        assertEquals(
+                0,
+                MainPart2.messageHashes.size());
+    }
+
+    @Test
+    public void testDisplayReportData() {
+
+        MainPart2.messageHashes.clear();
+        MainPart2.recipients.clear();
+        MainPart2.storedMessages.clear();
+
+        MainPart2.messageHashes.add(
+                "66:1:DIDCAKE");
+
+        MainPart2.recipients.add(
+                "+27838884567");
+
+        MainPart2.storedMessages.add(
+                "Did you get the cake?");
+
+        assertEquals(
+                "Did you get the cake?",
+                MainPart2.storedMessages.get(0));
     }}
